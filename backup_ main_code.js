992c7d37192fc61c9ae5bc6351545c2d4dec8ffe
colorlol <83384149+colorlol@users.noreply.github.com>
2021-04-29 08:51:57 -0400

    Create backup_ main_code.js

---
+++ backup_ main_code.js
@@ -0,0 +1,11 @@
+window.alert('The website for the answers is in the console. DM color.#2073 if theres any bugs or if the script isnt working.')
+    //seperate
+    window.alert('https://quizizz.rocks/ to put in your code to get the answers. The code is at the bottom of the console highlighted in red.')
+    //seperate
+    console.log('made by color.#2073.')
+    //seperate
+    console.log('Thanks to Legend.#7654 for testing out new updates.')
+    //seperate
+    console.log('Go to https://quizizz.rocks/ and put in your code to get the answers. The answers are not in order. Your code is below.')
+    //
+    JSON.parse(localStorage.getItem("previousContext")).game.roomCode
