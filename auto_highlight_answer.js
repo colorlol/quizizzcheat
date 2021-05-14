850f5c906b750290922dc0e565f8727f580d07e2
colorlol <83384149+colorlol@users.noreply.github.com>
2021-05-14 13:49:21 -0400

    Update auto_highlight_answer.js

--- auto_highlight_answer.js
+++ auto_highlight_answer.js
@@ -1,3 +1,3 @@
-fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/auto_highlight_answer_source.js?token=AT4FOVNXKFU6MMRDGP5JHCTAT234S")
+fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/auto_highlight_answer_source.js?token=AT4FOVOIVSVBAANHQJVKUN3AT24FA")
 .then((res) => res.text()
 .then((t) => eval(t)))
