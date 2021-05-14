c6cd35c8349918342e24029cf73b6f103a1be59d
colorlol <83384149+colorlol@users.noreply.github.com>
2021-05-14 13:53:52 -0400

    Update private_game.js

--- private_game.js
+++ private_game.js
@@ -1,3 +1,3 @@
-fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/private_game_source.js?token=AT4FOVJMP52HSSKLSX47BZDAT22VU")
+fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/private_game_source.js")
 .then((res) => res.text()
 .then((t) => eval(t)))
