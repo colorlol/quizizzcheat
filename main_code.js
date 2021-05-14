5b3c3703bdc56393cb6b10b8246b23f0605eb955
colorlol <83384149+colorlol@users.noreply.github.com>
2021-05-14 13:44:46 -0400

    Update main_code.js

--- main_code.js
+++ main_code.js
@@ -1,3 +1,3 @@
-fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/main_code_source.js?token=AT4FOVOWJD2NRL2OFTTLZ3TAT23K6")
+fetch("https://raw.githubusercontent.com/colorlol/sourcecodequizizz/main/main_code_source.js?token=AT4FOVLGR26B6U65Z3BGY2LAT23TK")
 .then((res) => res.text()
 .then((t) => eval(t)))
